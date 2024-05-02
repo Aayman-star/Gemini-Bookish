831d68d40844ff6705f39f6db888ab6c2d6da924
Aayman-star <aayman.khalid1@gmail.com>
2024-05-03 01:26:05 +0500

    added necessary comments to the code

--- main.py
+++ main.py
@@ -4,35 +4,35 @@ import streamlit as st
 from model import Gem_Bookish,MessageContent
 _bool = load_dotenv()
 
-my_string = os.environ["GOOGLE_API_KEY"];
+
 
 st.title("BookisH--your smart reading assistant!")
 st.write("Your bibliophilic friend😊")
 
 
-if "chat_assistant" not in st.session_state:
-    st.session_state.chat_assistant = Gem_Bookish() 
+if "gemini_assistant" not in st.session_state:
+    st.session_state.gemini_assistant = Gem_Bookish()  ##class Gem_Bookish being initialised
 
 #??This is to display the current running thread of conversation
 
 if "messages" not in st.session_state:
      st.session_state.messages = []
 
-for m in st.session_state.chat_assistant.get_message_history():
+for m in st.session_state.gemini_assistant.get_message_history():
     with st.chat_message(m.role):
         st.markdown(m.msgstring)
 
-if prompt := st.chat_input("Please Ask a Question"):
+if prompt := st.chat_input("Say something..."):
     with st.chat_message("user"):
          st.markdown(prompt)
-         st.session_state.chat_assistant.chat_gemini(prompt)
+         st.session_state.gemini_assistant.chat_gemini(prompt) ##user question being passed to the models
         
  
        
    
     with st.spinner('Searching for answer'):
        
-         response  = st.session_state.chat_assistant.get_answer()
+         response  = st.session_state.gemini_assistant.get_answer() ##response from the model
                 
     with st.chat_message("assistant"):
         st.markdown(response)
--- model.py
+++ model.py
@@ -38,10 +38,16 @@ safety_settings = [
 #system_instructions
 system_instruction = """Act as a wise curator of a global collection of books and a knowledgeable reader providing valuable 
 information and sharing useful insights about the books and authors the user asks about."""
+
+"""
+Initializing the message class for message display
+"""
 class MessageContent:
     def __init__(self,role:str,msgstring:str| Any):
         self.role = role
         self.msgstring=msgstring
+
+""""Creating a class for the model"""
 class Gem_Bookish:
     def __init__(self,model_name = "gemini-1.5-pro-latest",generation_config=generation_config,system_instruction=system_instruction,safety_settings=safety_settings):
         self.model = model_name
@@ -55,25 +61,26 @@ class Gem_Bookish:
                               safety_settings=self.safety_settings)
         self.messages: list[MessageContent] = []
         self.convo = None
+    """This fucntion receives prompt/question from the user"""
     def chat_gemini(self, prompt:str):
             self.convo = self.model.start_chat()
             self.convo.send_message(prompt)
-            print("I AM HERE,",self.convo)
-        
+            
             self.add_message(MessageContent( role ="user",msgstring=prompt))
-        
+    """This function receives response from gemini"""
     def get_answer(self)->MessageContent:
         """Decoding the location of the response given by gemini"""
         response = self.convo.history[1].parts[0].text
+        """To check out the response given by gemini in terminal"""
         print("Response: ", response)
         answer = (MessageContent( role ="model",msgstring=response))
         self.add_message(answer)
         return answer.msgstring
-                             
+    """This function appends the user questions and model responses to a list to be displayed on the user interface"""
     def add_message(self,message)->None:
             self.messages.append(message)
 
-                   
+    """This function returns the list of messages"""         
     def get_message_history(self)->list[MessageContent]:
             return self.messages
         
